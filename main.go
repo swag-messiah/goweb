a824059f8b49276d76bf33adcb48cad68aaf2ab7
Aleksandr Chizhikov <127817879+swag-messiah@users.noreply.github.com>
2023-09-07 07:44:11 +0300

    Update main.go

--- main.go
+++ main.go
@@ -6,7 +6,16 @@ type MyHandler struct {
 }
 
 func (MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Привет, сервер! Привет Мир!"))
+	if r.URL.Path == "/" {
+		w.Write([]byte("Home page"))
+		return
+	}
+	if r.URL.Path == "/hello" {
+		w.Write([]byte("Hello, user"))
+		return
+	}
+	w.WriteHeader(http.StatusNotFound)
+	w.Write([]byte("404 page not found"))
 }
 func main() {
 	http.ListenAndServe(":3000", MyHandler{})
